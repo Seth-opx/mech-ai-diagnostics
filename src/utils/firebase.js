55204640360afa3b3c68021b9e26278eaf502223
Seth <septe16187@gmail.com>
2026-05-20 17:20:29 +0200

    Add src/utils/firebase.js

---
+++ src/utils/firebase.js
@@ -0,0 +1,14 @@
+// Firebase configuration for Méco-IA
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAHnyyhSgDVbQKF-TbqDuStEDJ4K_7b4no",
+  authDomain: "my-mecoia.firebaseapp.com",
+  projectId: "my-mecoia",
+  storageBucket: "my-mecoia.firebasestorage.app",
+  messagingSenderId: "652511258912",
+  appId: "1:652511258912:web:275bbd8f549e4a1bea0f45",
+  measurementId: "G-ZRT3MC34YV"
+};
+
+export default firebaseConfig;
