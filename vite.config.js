bf1035814a830dcd7c33433dc8bb33295ce9afa7
Seth <septe16187@gmail.com>
2026-05-20 19:03:53 +0200

    Update ./vite.config.js from corrected code

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173
+  },
   build: {
     outDir: 'dist',
-    target: 'chrome100'
-  },
-  server: {
-    host: true,
-    port: 3000
+    sourcemap: false
   }
 })
