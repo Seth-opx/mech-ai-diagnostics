2667b10b21fc69e742b02876f9f2a32215c9e08f
Seth <septe16187@gmail.com>
2026-05-20 18:39:26 +0200

    Minimal test App for APK debug

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
-import ErrorBoundary from './components/ErrorBoundary'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ErrorBoundary>
-      <HashRouter>
-        <App />
-      </HashRouter>
-    </ErrorBoundary>
+    <App />
   </React.StrictMode>
 )
